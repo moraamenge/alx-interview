0c0d10e73c6dc15855ef92a39b9996f87f74336f
cindy <cynthiamenge96@gmail.com>
2022-12-22 14:41:26 +0300

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """ pascal triangle implementation
+    arg:
+        integer n
+    return:
+        list of lists
+    """
+
+    if n <= 0:
+        return []
+    pascalTriangle = [[1]]
+    if n <= 0:
+        return []
+
+    for i in range(n-1):
+        temp = [0]+pascalTriangle[-1]+[0]
+        arr = []
+        for j in range(len(temp) - 1):
+            arr.append(temp[j]+temp[j+1])
+        pascalTriangle.append(arr)
+    return pascalTriangle
