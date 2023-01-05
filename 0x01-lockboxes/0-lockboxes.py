f01da3a54b5acfd511b8f008c7d63245666ac7e3
cindy <cynthiamenge96@gmail.com>
2023-01-05 20:27:54 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Return True if all boxes can be opened, else return False """
+
+
+def canUnlockAll(boxes):
+    """ T.C O(n)  S.C O(n) """
+    keys = []
+    for box in boxes:
+        keys+=box
+    for i in range(len(boxes)):
+        if i+1 not in keys and i+1 < len(boxes):
+            return False
+    return True
